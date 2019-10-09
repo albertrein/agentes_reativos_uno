59d0e68adb6bbfe4a1ff1e8d7b253ae893e794df
guilhermear <guilhermear@fastcompras.com.br>
2019-10-09 13:50:23 -0300

    Iniciando modelagem agente.

---
+++ agente.js
@@ -0,0 +1,17 @@
+class Agente{
+	constructor(UNO, ambiente){
+		this.cartasNaMao = [];
+		//pega 7 cartas do baralho
+	}
+
+	//realizaJogada
+		//Observa ambiente
+		//procura cartas na lista de cartasNaMao
+	
+	//pegaNumeroCartas especifico de cartas do baralho
+
+	//--metodos-- dos agente
+//inicia jogo _construct(embaralha)
+//embaralhar
+//RemoveCarta (remove carta do baralho)
+}

69f2fefffe21dd09272042cb2608259830574efd
albertrein <guilhermealberto8@gmail.com>
2019-11-03 11:40:06 -0300

    Desenvolvendo metodo realiza jogada.

--- Agente.js
+++ Agente.js
@@ -25,13 +25,58 @@ class Agente{
 	mostrarCartasNaMao(){ this.pegaCartaDoBaralho(); console.log(this.cartasNaMao); }
 
 	async realizaJogada(){
-		//Observa ambiente
-			//Observa ultima carta jogada e realiza operação de acordo com ela
+		let ultimaCartaJogada = uno.getUltimaCartaJogadaNoAmbiente();
 
+		//Verifica se a última carta foi uma coringa e retorna
+		if(ultimaCartaJogada.tipo === 'coringa'){
+			switch(ultimaCartaJogada.acao){
+				case 'pular':
+					break;
+				case 'reverter':
+					break;
+				case '+2':
+					this.pegaCartaDoBaralho(2);
+					break;
+				case '+4':
+					this.pegaCartaDoBaralho(4);					
+			}
+			return;
+		}
+
+		
+		//Encontra uma carta para jogar
+		this.cartasNaMao.forEach((carta, chave) => {
+			if( (carta.tipo === 'numerada' && ((carta.cor === ultimaCartaJogada.cor) || (carta.id === ultimaCartaJogada.id)))  || carta.tipo === 'coringa' ){
+				uno.insereCartaNoAmbiente(carta); //Inserindo carta
+				this.cartasNaMao.splice(chave, chave); //Remove carta da mão
+				this.verificaGanhador(); // retorna throw se acabou as cartas
+				return;
+			}
+		});
+
+		//Não encontrou cartas na mão
+		//retirar cartas do baralho até encontrar
+		while(true){
+			let novaCarta = await uno.retiraCartaDoBaralho().catch((erro) => {throw erro});
+
+			if( (novaCarta.tipo === 'numerada' && ((novaCarta.cor === ultimaCartaJogada.cor) || (novaCarta.id === ultimaCartaJogada.id)))  || novaCarta.tipo === 'coringa' ){
+				uno.insereCartaNoAmbiente(carta); //Inserindo carta				
+				return;
+			}else{
+				insereCartaNaMao(novaCarta);
+			}
+
+		}
+	}
+
+	verificaGanhador(){
 		if(this.cartasNaMao.length <= 0){
 			thrown this.nomeAgente + ' Ganhou!';
 		}
+	}
 
+	insereCartaNaMao(carta){
+		this.cartasNaMao.push(carta);
 	}
 
 	/*
